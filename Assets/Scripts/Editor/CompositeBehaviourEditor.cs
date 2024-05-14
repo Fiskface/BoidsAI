514c67f6c8e79e2d8d76cee4c709df9575a2b90c
Lukas <lukas.almgren03@gmail.com>
2024-05-14 15:52:48 +0200

    YIPPIEEEE!

--- Assets/Scripts/Editor/CompositeBehaviourEditor.cs
+++ Assets/Scripts/Editor/CompositeBehaviourEditor.cs
@@ -8,66 +8,48 @@ public class CompositeBehaviourEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        //setup
         CompositeBehaviour cb = (CompositeBehaviour)target;
 
-        Rect r = EditorGUILayout.BeginHorizontal();
-        r.height = EditorGUIUtility.singleLineHeight;
-
+        //check for behaviors
         if (cb.behaviours == null || cb.behaviours.Length == 0)
         {
-            EditorGUILayout.HelpBox("No behaviours in array.", MessageType.Warning);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.HelpBox("No behaviors in array.", MessageType.Warning);
             EditorGUILayout.EndHorizontal();
-            r = EditorGUILayout.BeginHorizontal();
-            r.height += EditorGUIUtility.singleLineHeight;
         }
         else
         {
-            r.x = 30f;
-            r.width = EditorGUIUtility.currentViewWidth - 95f;
-            EditorGUI.LabelField(r, "Behaviours");
-            r.x = EditorGUIUtility.currentViewWidth - 65f;
-            r.width = 60f;
-            EditorGUI.LabelField(r, "Weights");
-            r.y += EditorGUIUtility.singleLineHeight * 1.2f;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Number", GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+            EditorGUILayout.LabelField("Behaviors", GUILayout.MinWidth(60f));
+            EditorGUILayout.LabelField("Weights", GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+            EditorGUILayout.EndHorizontal();
 
-            EditorGUI.BeginChangeCheck();
             for (int i = 0; i < cb.behaviours.Length; i++)
             {
-                r.x = 5f;
-                r.width = 20f;
-                EditorGUI.LabelField(r, i.ToString());
-                r.x = 30f;
-                r.width = EditorGUIUtility.currentViewWidth - 95f;
-                cb.behaviours[i] = (FlockBehaviour)EditorGUI.ObjectField(r, cb.behaviours[i], typeof(FlockBehaviour), false);
-                r.x = EditorGUIUtility.currentViewWidth - 65f;
-                r.width = 60f;
-                cb.weights[i] = EditorGUI.FloatField(r, cb.weights[i]);
-                r.y += EditorGUIUtility.singleLineHeight * 1.1f;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(i.ToString(), GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+                cb.behaviours[i] = (FlockBehaviour)EditorGUILayout.ObjectField(cb.behaviours[i], typeof(FlockBehaviour), false, GUILayout.MinWidth(60f));
+                cb.weights[i] = EditorGUILayout.FloatField(cb.weights[i], GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+                EditorGUILayout.EndHorizontal();
             }
-            if (EditorGUI.EndChangeCheck()) EditorUtility.SetDirty(cb);
         }
 
-        EditorGUILayout.EndHorizontal();
-        r.x = 5f;
-        r.width = EditorGUIUtility.currentViewWidth - 10f;
-        r.y += EditorGUIUtility.singleLineHeight * 0.5f;
-        if (GUI.Button(r, "Add Behaviour"))
+        if (GUILayout.Button("Add Behaviour"))
         {
-            Debug.Log("Hej");
             AddBehaviour(cb);
             EditorUtility.SetDirty(cb);
         }
 
-        r.y += EditorGUIUtility.singleLineHeight * 1.5f;
-        if(cb.behaviours != null && cb.behaviours.Length > 0)
+        if (cb.behaviours != null && cb.behaviours.Length > 0)
         {
-            if(GUI.Button(r, "Remove Behaviour"))
+            if (GUILayout.Button("Remove Behaviour"))
             {
                 RemoveBehaviour(cb);
                 EditorUtility.SetDirty(cb);
             }
         }
-
     }
 
     void AddBehaviour(CompositeBehaviour cb)
